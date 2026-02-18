d6d30580aec845b658cbd166f1a0ec5b773a9e2f
hannahashe <hannahashe13@gmail.com>
2026-02-18 18:05:23 +0000

    Add EventDetailView import to urls.py

--- circleevents/urls.py
+++ circleevents/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import EventListView
+from .views import EventListView, EventDetailView
 
 urlpatterns = [
     path("", EventListView.as_view(), name="event_list"),
