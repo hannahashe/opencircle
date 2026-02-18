74651faa9297026f743449142f43cc78a107165e
hannahashe <hannahashe13@gmail.com>
2026-02-18 16:49:09 +0000

    Refactor docstring formatting in EventListView for improved readability

--- circleevents/views.py
+++ circleevents/views.py
@@ -9,18 +9,20 @@ from .models import Event
 class EventListView(ListView):
     """
     This view is responsible for displaying a list of events.
-     It uses the Event model to retrieve the events from the database.
-     The template used to render the list of events is "circleevents/event_list.html".
-     The context variable that will be used in the template to access the list of events is "events".
+    It uses the Event model to retrieve the events from the database.
+    The template used to render the list of events
+    is "circleevents/event_list.html".
+    The context variable that will be used in 
+    the template to access the list of events is "events".
     """
     model = Event
     template_name = "circleevents/event_list.html"
     context_object_name = "events"
     """
-    Override the get_queryset method to filter events 
+    Override the get_queryset method to filter events
     based on their status and published date.
-    Only events with a status of "approved" and 
-    a published date that is less than or 
+    Only events with a status of "approved" and
+    a published date that is less than or
     equal to the current time will be included in the queryset.
     The events will be ordered by their published date in descending order.
     """
