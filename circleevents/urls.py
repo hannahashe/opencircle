e6d73152168dc18556a8798f600e19aa13ca80e4
hannahashe <hannahashe13@gmail.com>
2026-02-18 18:05:59 +0000

    Add URL pattern for EventDetailView to handle individual event details

--- circleevents/urls.py
+++ circleevents/urls.py
@@ -3,4 +3,5 @@ from .views import EventListView, EventDetailView
 
 urlpatterns = [
     path("", EventListView.as_view(), name="event_list"),
+    path("<slug:slug>/", EventDetailView.as_view(), name="event_detail"),
 ]
