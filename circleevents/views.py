48bc22cc4b6f68b587efa8eae1c8729d5e9a8afa
hannahashe <hannahashe13@gmail.com>
2026-02-18 16:47:27 +0000

    Enhance documentation for EventListView with detailed class and method descriptions

--- circleevents/views.py
+++ circleevents/views.py
@@ -5,12 +5,28 @@ from django.utils import timezone
 
 from .models import Event
 
+# Events List View below, using Django's generic ListView to
+# display approved events only from ordered by published date.
+
 
 class EventListView(ListView):
+    """
+    This view is responsible for displaying a list of events.
+     It uses the Event model to retrieve the events from the database.
+     The template used to render the list of events is "circleevents/event_list.html".
+     The context variable that will be used in the template to access the list of events is "events".
+    """
     model = Event
     template_name = "circleevents/event_list.html"
     context_object_name = "events"
-
+    """
+    Override the get_queryset method to filter events 
+    based on their status and published date.
+    Only events with a status of "approved" and 
+    a published date that is less than or 
+    equal to the current time will be included in the queryset.
+    The events will be ordered by their published date in descending order.
+    """
     def get_queryset(self):
         return Event.objects.filter(
             status="approved",
