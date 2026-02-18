6f5cdf0770c7758f17ab53812b530e505ba27845
hannahashe <hannahashe13@gmail.com>
2026-02-18 20:16:16 +0000

    Add profile view URL pattern to event URLs

--- circleevents/urls.py
+++ circleevents/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import EventListView, EventDetailView
+from .views import EventListView, EventDetailView, profile_view
+
 
 urlpatterns = [
     path("", EventListView.as_view(), name="event_list"),
+    path("profile/", profile_view, name="profile"),
     path("<slug:slug>/", EventDetailView.as_view(), name="event_detail"),
 ]
