a8f11b8466a0ed98bc6feed39d73f3d1afe39294
hannahashe <hannahashe13@gmail.com>
2026-02-18 20:51:06 +0000

    Add organiser_test URL path to urlpatterns

--- circleevents/urls.py
+++ circleevents/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import EventListView, EventDetailView, profile_view
+from .views import EventListView, EventDetailView, profile_view, organiser_test_view
 
 
 urlpatterns = [
     path("", EventListView.as_view(), name="event_list"),
     path("profile/", profile_view, name="profile"),
+    path("organiser-test/", organiser_test_view, name="organiser_test"),
     path("<slug:slug>/", EventDetailView.as_view(), name="event_detail"),
 ]
