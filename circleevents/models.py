72f9789b8892e7b568b44613457746d922515a6e
hannahashe <hannahashe13@gmail.com>
2026-02-18 12:36:21 +0000

    Add slug generation logic to Event model's save method

--- circleevents/models.py
+++ circleevents/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.text import slugify
 
 
 class Profile(models.Model):
@@ -57,6 +58,20 @@ class Event(models.Model):
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            base_slug = slugify(self.title)
+            slug = base_slug
+            counter = 1
+
+        while Event.objects.filter(slug=slug).exists():
+            slug = f"{base_slug}-{counter}"
+            counter += 1
+
+        self.slug = slug
+
+    super().save(*args, **kwargs)
 
 
 class Notification(models.Model):
