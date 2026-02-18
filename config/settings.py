77521b040362c538641330be13f9f797d50dca00
hannahashe <hannahashe13@gmail.com>
2026-02-18 11:33:15 +0000

    Refactor settings.py to load environment variables from env.py and secure SECRET_KEY

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 
+import os
+
+if os.path.isfile("env.py"):
+    import env
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1wcb2xlzc7gd*z_!_j4%73%4_!f@vu1#b_3^3*)*nrk%y9vo7$'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
