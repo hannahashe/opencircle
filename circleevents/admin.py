2ed2e44be35e222fcdb389fbdd2554ec21ee5c6f
hannahashe <hannahashe13@gmail.com>
2026-02-18 11:41:16 +0000

    Remove commented line and organize model registrations in admin.py

--- circleevents/admin.py
+++ circleevents/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Event, Profile, Notification
 
-# Register your models here.
+
+admin.site.register(Event)
+admin.site.register(Profile)
+admin.site.register(Notification)
