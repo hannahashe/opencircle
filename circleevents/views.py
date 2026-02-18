4ad2c931e6539dbc7b88831554c8bdfbeb9f22b1
hannahashe <hannahashe13@gmail.com>
2026-02-18 16:43:07 +0000

    Add EventListView to display approved events in the event list template

--- circleevents/views.py
+++ circleevents/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import ListView
+from django.utils import timezone
+
+from .models import Event
+
+
+class EventListView(ListView):
+    model = Event
+    template_name = "circleevents/event_list.html"
+    context_object_name = "events"
+
+    def get_queryset(self):
+        return Event.objects.filter(
+            status="approved",
+            published_at__lte=timezone.now()
+        ).order_by("-published_at")
