fcffe32551d4c51069614dcd4da32bf7348c57da
hannahashe <hannahashe13@gmail.com>
2026-02-18 16:52:36 +0000

    Enhance docstring for create_profile signal receiver in signals.py for better clarity

--- circleevents/signals.py
+++ circleevents/signals.py
@@ -7,5 +7,16 @@ from .models import Profile
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
+    """
+    This signal receiver function is triggered after a User instance is saved.
+    If the User instance is newly created (created=True),
+    it creates a corresponding Profile instance
+    associated with the User instance.
+    Args:
+        sender: The model class that sent the signal (User).
+        instance: The actual instance of the model that was saved.
+        created: A boolean indicating whether a new record was created.
+        **kwargs: Additional keyword arguments.
+    """
     if created:
         Profile.objects.create(user=instance)
