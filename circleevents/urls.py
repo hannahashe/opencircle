a8644d7f164785f99769b9d1d25deb2438c04130
hannahashe <hannahashe13@gmail.com>
2026-02-19 16:54:16 +0000

    Reorder import statements and move EventDetailView path below edit_event for improved readability

--- circleevents/urls.py
+++ circleevents/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
-from .views import EventListView, EventDetailView, profile_view, create_event, edit_event
+from .views import (
+    EventListView,
+    EventDetailView,
+    profile_view,
+    create_event,
+    edit_event
+)
 
 
 urlpatterns = [
     path("", EventListView.as_view(), name="event_list"),
     path("profile/", profile_view, name="profile"),
     path("create/", create_event, name="create_event"),
-    path("<slug:slug>/", EventDetailView.as_view(), name="event_detail"),
     path("<slug:slug>/edit/", edit_event, name="edit_event"),
+    path("<slug:slug>/", EventDetailView.as_view(), name="event_detail"),
 ]
