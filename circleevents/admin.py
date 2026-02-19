413f1ed66848a6b310588bb7f0f0110edab85055
hannahashe <hannahashe13@gmail.com>
2026-02-19 17:45:53 +0000

    Refactor Event registration in admin panel to use EventAdmin class with improved display and filtering options

--- circleevents/admin.py
+++ circleevents/admin.py
@@ -2,6 +2,29 @@ from django.contrib import admin
 from .models import Event, Profile, Notification
 
 
-admin.site.register(Event)
 admin.site.register(Profile)
 admin.site.register(Notification)
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "status",
+        "organiser",
+        "start_datetime",
+        "published_at",
+    )
+
+    list_filter = (
+        "status",
+        "start_datetime",
+    )
+
+    search_fields = (
+        "title",
+        "organiser__username",
+        "city",
+        "postcode",
+    )
+
+    ordering = ("-created_at",)
