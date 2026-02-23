0fe31df3a213d7e66d26074fc1a25f5f6eb5ab98
hannahashe <hannahashe13@gmail.com>
2026-02-23 18:10:54 +0000

    Add HomeView URL pattern for the home page

--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from circleevents.views import HomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", HomeView.as_view(), name="home"),
     path("accounts/", include("allauth.urls")),
     path("events/", include("circleevents.urls")),
+
 ]
