40d0d2d10518c046e35138fc1e2ca2c9d3d610e1
hannahashe <hannahashe13@gmail.com>
2026-02-23 18:15:50 +0000

    Add featured events to HomeView context

--- circleevents/views.py
+++ circleevents/views.py
@@ -15,6 +15,13 @@ from .models import Event
 class HomeView(TemplateView):
     template_name = "circleevents/home.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["featured_events"] = Event.objects.filter(
+            status="approved"
+        ).order_by("start_datetime")[:3]
+        return context
+
 
 class EventListView(ListView):
     """
