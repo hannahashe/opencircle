41bd163b4168c92b138b03f5090ee98bb3f08d16
hannahashe <hannahashe13@gmail.com>
2026-02-24 22:35:37 +0000

    fix: remove hardcoded ALLOWED_HOSTS to allow heroku ALLOWED_HOSTS to stay set during deployment

--- config/settings.py
+++ config/settings.py
@@ -25,11 +25,6 @@ DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
-ALLOWED_HOSTS = [
-    "localhost",
-]
-
-
 # Application definition
 
 INSTALLED_APPS = [
